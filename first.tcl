711ea3ef7f0186689e3c7f006128184301b5274e
Ptilopsixuan <ptilopsixu_n@outlook.com>
2025-12-01 17:32:29 +0800

    2element

--- first.tcl
+++ first.tcl
@@ -1,3 +1,25 @@
 set A 1
 puts {$A\n}
 puts "$A\n"
+
+model basic -ndm 2 -ndf 3
+
+node 1 0.0 0.0
+node 2 1.0 0.0
+node 3 0.0 1.0
+node 4 1.0 1.0
+
+fix 1 1 1 1
+fix 2 1 1 1
+fix 3 0 0 0
+fix 4 0 0 0
+
+mass 3 1 0 0
+mass 4 1 0 0
+
+geomTransf Linear 1
+
+element elasticBeamColumn 1 1 3 3000.0 300000.0 1
+element elasticBeamColumn 2 3 4 3000.0 300000.0 1
+element elasticBeamColumn 3 2 4 3000.0 300000.0 1
+
